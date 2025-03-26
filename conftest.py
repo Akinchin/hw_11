9a130b181da360d218d9ffbf0a1c091fbd0592d6
user.email <wpk092@mail.ru>
2025-03-26 10:58:55 +0300

    изменения 3

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 
 import pytest
+from dotenv import load_dotenv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,13 +8,34 @@ from selene import Browser, Config
 
 from utils import attach
 
+DEFAULT_BROWSER_VERSION = "100.0"
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+def load_dotenv():
+    load_dotenv()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "128.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -25,7 +47,7 @@ def setup_browser(request):
         options=options
     )
 
-    browser = Browser(Config(driver))
+    browser = Browser(Config(driver=driver))
     yield browser
 
     attach.add_screenshot(browser)
